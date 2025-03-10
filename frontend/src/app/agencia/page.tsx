5fe5f39d98634246b47fc21749a03bd087dd9e5d
JoaoVitorGirardii <joao.vitor.girardii@gmail.com>
2025-03-10 20:43:47 -0300

    criado tabela padrao

--- frontend/src/app/agencia/page.tsx
+++ frontend/src/app/agencia/page.tsx
@@ -1,107 +1,33 @@
 'use client'
 
 import { useState, useEffect } from 'react'
-import { Card, CardContent } from '@/components/ui/card'
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
-import { Checkbox } from '@/components/ui/checkbox'
-import {
-    useReactTable,
-    getCoreRowModel,
-    getSortedRowModel,
-    getPaginationRowModel,
-    flexRender,
-    SortingState,
-    ColumnDef,
-} from '@tanstack/react-table'
+
 import VerticalMenu from '../../components/MenuVertical'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCheck, faArrowDown, faArrowUp } from '@fortawesome/free-solid-svg-icons'
 import { CabecalhoTelaConsulta } from '@/components/CabecalhoTelaConsulta'
 import { AgenciaService } from '@/services/agencia/agenciaService'
 import { AgenciaDTO } from '@/dto/agencia.dto'
+import { CriaColunasProps } from '@/components/tabela_padrao/criaColunas'
+import { TabelaPadrao } from '@/components/tabela_padrao/tabelaPadrao'
 
-const columns: ColumnDef<AgenciaDTO>[] = [
-    {
-        id: 'select',
-        header: ({ table }) => (
-            <Checkbox checked={table.getIsAllPageRowsSelected()} onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)} />
-        ),
-        cell: ({ row }) => <Checkbox checked={row.getIsSelected()} onCheckedChange={(value) => row.toggleSelected(!!value)} />,
-    },
-    {
-        accessorKey: 'id',
-        header: ({ column }) => (
-            <button
-                className="flex items-center hover:bg-gray-100 p-2 rounded"
-                onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
-            >
-                ID
-                {column.getIsSorted() === 'asc' ? (
-                    <FontAwesomeIcon icon={faArrowUp} className="ml-2 h-4 w-4" />
-                ) : (
-                    <FontAwesomeIcon icon={faArrowDown} className="ml-2 h-4 w-4" />
-                )}
-            </button>
-        ),
-    },
-    {
-        accessorKey: 'banco.nome',
-        header: ({ column }) => (
-            <button
-                className="flex items-center hover:bg-gray-100 p-2 rounded"
-                onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
-            >
-                Nome do Banco
-                {column.getIsSorted() === 'asc' ? (
-                    <FontAwesomeIcon icon={faArrowUp} className="ml-2 h-4 w-4" />
-                ) : (
-                    <FontAwesomeIcon icon={faArrowDown} className="ml-2 h-4 w-4" />
-                )}
-            </button>
-        ),
-    },
-    {
-        accessorKey: 'agencia',
-        header: ({ column }) => (
-            <button
-                className="flex items-center hover:bg-gray-100 p-2 rounded"
-                onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
-            >
-                Agência
-                {column.getIsSorted() === 'asc' ? (
-                    <FontAwesomeIcon icon={faArrowUp} className="ml-2 h-4 w-4" />
-                ) : (
-                    <FontAwesomeIcon icon={faArrowDown} className="ml-2 h-4 w-4" />
-                )}
-            </button>
-        ),
-    },
-    {
-        accessorKey: 'ativo',
-        header: ({ column }) => (
-            <button
-                className="flex items-center hover:bg-gray-100 p-2 rounded"
-                onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
-            >
-                Ativo
-                {column.getIsSorted() === 'asc' ? (
-                    <FontAwesomeIcon icon={faArrowUp} className="ml-2 h-4 w-4" />
-                ) : (
-                    <FontAwesomeIcon icon={faArrowDown} className="ml-2 h-4 w-4" />
-                )}
-            </button>
-        ),
-        cell: ({ row }) => (
-            <div className="flex justify-center">
-                {row.original.ativo ? <FontAwesomeIcon icon={faCheck} className="text-black" /> : null}
-            </div>
-        ),
-    },
+const columns: CriaColunasProps = {
+    selectVisible: true,
+    colunmAtivo: true,
+    colunas: [
+        { accessorKey: 'id', label: 'ID' },
+        { accessorKey: 'banco.nome', label: 'Nome do Banco' },
+        { accessorKey: 'agencia', label: 'Agência' },
+    ],
+}
+
+const colunasStyle = [
+    { id: 'select', style: 'text-center w-[35px]' },
+    { id: 'id', style: 'text-right w-[80px]' },
+    { id: 'nome_banco', style: 'text-left' },
+    { id: 'agencia', style: 'text-left' },
+    { id: 'ativo', style: 'text-center w-[80px]' },
 ]
 
 export default function AgenciaConsulta() {
-    const [rowSelection, setRowSelection] = useState({})
-    const [sorting, setSorting] = useState<SortingState>([])
     const [data, setData] = useState<AgenciaDTO[]>([])
 
     useEffect(() => {
@@ -117,38 +43,6 @@ export default function AgenciaConsulta() {
         fetchData()
     }, [])
 
-    const table = useReactTable({
-        data,
-        columns,
-        state: {
-            rowSelection,
-            sorting,
-        },
-        onRowSelectionChange: setRowSelection,
-        onSortingChange: setSorting,
-        getCoreRowModel: getCoreRowModel(),
-        getSortedRowModel: getSortedRowModel(),
-        getPaginationRowModel: getPaginationRowModel(),
-    })
-
-    function colunmStyle(columnName: string) {
-        let className = 'px-4 py-2'
-
-        if (columnName === 'select') {
-            className += ' text-center w-[35px]'
-        } else if (columnName === 'id') {
-            className += ' text-right w-[80px]'
-        } else if (columnName === 'nome_banco') {
-            className += ' text-left'
-        } else if (columnName === 'agencia') {
-            className += ' text-left'
-        } else if (columnName === 'ativo') {
-            className += ' text-center w-[80px]'
-        }
-
-        return className
-    }
-
     return (
         <div className="flex h-screen overflow-hidden">
             <VerticalMenu />
@@ -159,38 +53,7 @@ export default function AgenciaConsulta() {
                 </div>
                 <CabecalhoTelaConsulta href="agencia" />
 
-                <Card>
-                    <CardContent className="p-4">
-                        <Table>
-                            <TableHeader>
-                                {table.getHeaderGroups().map((headerGroup) => (
-                                    <TableRow key={headerGroup.id}>
-                                        {headerGroup.headers.map((header) => {
-                                            return (
-                                                <TableHead key={header.id} className={colunmStyle(header.column.id)}>
-                                                    {flexRender(header.column.columnDef.header, header.getContext())}
-                                                </TableHead>
-                                            )
-                                        })}
-                                    </TableRow>
-                                ))}
-                            </TableHeader>
-                            <TableBody>
-                                {table.getRowModel().rows.map((row) => (
-                                    <TableRow key={row.id}>
-                                        {row.getVisibleCells().map((cell) => {
-                                            return (
-                                                <TableCell key={cell.id} className={colunmStyle(cell.column.id)}>
-                                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                                                </TableCell>
-                                            )
-                                        })}
-                                    </TableRow>
-                                ))}
-                            </TableBody>
-                        </Table>
-                    </CardContent>
-                </Card>
+                <TabelaPadrao data={data} colunas={columns} colunasStyle={colunasStyle} />
             </div>
         </div>
     )
